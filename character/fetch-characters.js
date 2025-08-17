adea69755f094ae35f99f5775fdcd89aa66cfe1b
Tsegaw-Mao <tsegawjohnj@gmail.com>
2025-08-17 15:23:06 +0300

    task 0

---
+++ character/fetch-characters.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+const url = "https://rickandmortyapi.com/graphql";
+
+// Character IDs we want
+const ids = [1, 2, 3, 4];
+
+async function fetchCharacter(id) {
+  const query = fs.readFileSync(`character-id-${id}.graphql`, "utf8");
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query }),
+  });
+
+  const data = await response.json();
+  fs.writeFileSync(`character-id-${id}-output.json`, JSON.stringify(data, null, 2));
+  console.log(`Saved character-id-${id}-output.json`);
+}
+
+async function main() {
+  for (const id of ids) {
+    await fetchCharacter(id);
+  }
+}
+
+main();
